e94b5e211b30a298b385adfcbab0ea0c2fb46584
lorrannyfigueredo <lorrannyfigueredo3@gmail.com>
2022-02-09 20:17:04 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+
+
 function App() {
   return (
     <div className="app">
-      <h1>App</h1>
+      <LoginPage />
     </div>
   );
 }
